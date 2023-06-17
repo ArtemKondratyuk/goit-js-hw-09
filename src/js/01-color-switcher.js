67f04453baf9cd6e0075a3aa895cd2b98efed555
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2023-06-17 09:43:47 +0300

    update js

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,19 @@
+const btnStart=document.querySelector('button[data-start]');
+const btnStop=document.querySelector('button[data-stop]');
+let intervalId =null;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  btnStop.disabled=true; 
+btnStart.addEventListener('click', ()=>{
+  btnStart.disabled=true;
+    intervalId=setInterval(()=>{
+    btnStart.parentNode.style.backgroundColor = getRandomHexColor()
+    btnStop.disabled=false;
+},1000);
+});
+
+btnStop.addEventListener('click',()=>{
+btnStart.disabled=false;
+clearInterval(intervalId);
+})
