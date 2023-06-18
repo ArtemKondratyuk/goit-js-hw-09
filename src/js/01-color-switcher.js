6cef1baa0c9be0012498a039c57e524774466115
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2023-06-18 13:21:32 +0300

    update js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,23 @@
-const btnStart=document.querySelector('button[data-start]');
-const btnStop=document.querySelector('button[data-stop]');
-let intervalId =null;
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let intervalId = null;
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
-  btnStop.disabled=true; 
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+btnStop.disabled = true;
 
-btnStart.addEventListener('click', ()=>{
-  btnStart.disabled=true;
-    intervalId=setInterval(()=>{
-    btnStart.parentNode.style.backgroundColor = getRandomHexColor()
-    btnStop.disabled=false;
-},1000);
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  intervalId = setInterval(() => {
+    btnStart.parentNode.style.backgroundColor = getRandomHexColor();
+    btnStop.disabled = false;
+  }, 1000);
 });
 
-btnStop.addEventListener('click',()=>{
-btnStart.disabled=false;
-clearInterval(intervalId);
-})
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  clearInterval(intervalId);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,13 +4,13 @@ function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({position, delay});
+        resolve({ position, delay });
       } else {
-        reject({position, delay});
+        reject({ position, delay });
       }
     }, delay);
   });
-};
+}
 const formEl = document.querySelector('form');
 const delayEl = document.querySelector('input[name="delay"]');
 const stepEl = document.querySelector('input[name="step"]');
@@ -18,18 +18,22 @@ const amountEl = document.querySelector('input[name="amount"]');
 formEl.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
-const delay = parseInt(delayEl.value);
+  const delay = parseInt(delayEl.value);
   const step = parseInt(stepEl.value);
   const amount = parseInt(amountEl.value);
-let currentDelay = delay;
-for (let i = 1; i <= amount; i++) {
+  let currentDelay = delay;
+  for (let i = 1; i <= amount; i++) {
     createPromise(i, currentDelay)
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
-currentDelay += step;
+    currentDelay += step;
   }
 }
